cd8bc3c3a82cad3f2dd1e82d820889490228d62d
zamin002 <148207180+zamin002@users.noreply.github.com>
2025-03-19 01:08:39 +0000

    Add files

---
+++ Week 4/Code snippets/import socket.py
@@ -0,0 +1,26 @@
+
+import socket
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+server_address = ("localhost", 65433)
+
+user = input("Enter username: ")
+pw = input("Enter password: ")
+print(f"sending {user}, {pw}")
+client_socket.sendto(f"{user},{pw}".encode(), server_address)
+
+response,_ = client_socket.recvfrom(2048)
+print(response.decode())
+
+if "successful" not in response.decode():
+    print("Failed login attempt. Goodbye.")
+    exit()
+
+while True:
+    message = input("Client: ")
+    #send message to server
+    client_socket.sendto(message.encode(), server_address)
+
+    reply,_ = client_socket.recvfrom(2048)
+    
